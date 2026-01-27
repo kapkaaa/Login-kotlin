3091752c10c1fb36c983454a0986ff607cfeb7a2
Kafka Ahmad Sanjaya <andamanusia03@gmail.com>
2026-01-27 17:06:22 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/learning2/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.learning2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    val  username: String = "kafka"
+    val  password: String = "password"
+    private lateinit var binding: ActivityMainBinding
+
+    lateinit var usn: txt_usn
+    lateinit var pw: txt_pw
+    lateinit var login: button
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+    }
+}
